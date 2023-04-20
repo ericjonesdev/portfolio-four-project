1385146f2e16a26bccc786b5d2a8fa9836bf45bb
ericjonesdev <ericjones2004@yahoo.com>
2023-04-20 17:50:32 +0000

    Edit allowed hosts

--- django_newssite/settings.py
+++ django_newssite/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django_newssite.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['a-coders-journey.herokuapp.com', 'localhost']
 
 
 # Application definition
