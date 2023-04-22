81fbeff436ebcd94ca87bffdf09084174006c624
ericjonesdev <ericjones2004@yahoo.com>
2023-04-22 20:12:35 +0000

    Add template and static folder files - account folder

--- blog_site/views.py
+++ blog_site/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
 
-def get_homepage(request):
-    return render(request, 'blog_site/blog_site_list.html')
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 6
+
+
+
