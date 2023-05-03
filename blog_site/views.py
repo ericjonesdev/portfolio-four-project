ed11e734bf735040b5fe02510802ddd37234bb7c
ericjonesdev <>
2023-05-03 11:07:56 +0000

    Edit PostList view to filter by (status='Published') to have all blog post show on index.html

--- blog_site/views.py
+++ blog_site/views.py
@@ -29,7 +29,7 @@ class UserProfileView(DetailView):
 
 class PostList(generic.ListView):
     model = Post
-    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    queryset = Post.objects.filter(status='Published').order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 6
 
