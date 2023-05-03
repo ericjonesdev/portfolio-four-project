12d6bab105827591577c859dbcbcac98c8d2e67b
ericjonesdev <>
2023-05-03 11:17:30 +0000

    Edit PostDetail view to filter on status='Published' to allow for user post link urs to properly display/work

--- blog_site/views.py
+++ blog_site/views.py
@@ -37,7 +37,7 @@ class PostList(generic.ListView):
 class PostDetail(View):
 
     def get(self, request, slug, *args, **kwargs):
-        queryset = Post.objects.filter(status=1)
+        queryset = Post.objects.filter(status='Published')
         post = get_object_or_404(queryset, slug=slug)
         comments = post.comments.filter(approved=True).order_by("created_on")
         liked = False
