c9c4a6dc32ee27b1fb9ffb218cb8aa7114340cec
ericjonesdev <>
2023-05-03 11:31:46 +0000

    edit PostAdmin to also list the author name

--- blog_site/admin.py
+++ blog_site/admin.py
@@ -9,7 +9,7 @@ class PostAdmin(SummernoteModelAdmin):
 
     prepopulated_fields = {'slug': ('title',)}
     list_filter = ('status', 'created_on')
-    list_display = ('title', 'slug', 'status', 'created_on')
+    list_display = ('title', 'author', 'slug', 'status', 'created_on')
     search_fields = ['title', 'content']
     summernote_fields = ('content')
 
