79b68b77900d56e04913abb47f3de90468665ba4
ericjonesdev <>
2023-05-05 12:21:46 +0000

    edit conn statement in settings.py to correct syntax error

--- django_newssite/settings.py
+++ django_newssite/settings.py
@@ -111,7 +111,8 @@ DATABASES = {
      'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
  }
 
-conn = psycopg2.connect(DATABASES, sslmode='require')
+conn = psycopg2.connect(**DATABASES['default'], sslmode='require')
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
