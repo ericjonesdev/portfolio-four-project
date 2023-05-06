3fe2739bc9af653177b3b1087317379b9731d275
ericjonesdev <>
2023-05-06 20:41:40 +0000

    edit cloudinary

--- django_newssite/settings.py
+++ django_newssite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 from pathlib import Path
+import cloudinary
 import cloudinary_storage
 import os
 import dj_database_url
@@ -17,7 +18,6 @@ if os.path.isfile('env.py'):
     import env
 
 development = os.environ.get('DEVELOPMENT', False)
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
